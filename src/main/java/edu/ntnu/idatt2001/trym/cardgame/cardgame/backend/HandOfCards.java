892e7223660ab26f1f68874aeeb607bb6389af45
Trym Gudvangen <trym.gudvangen@gmail.com>
2022-03-26 16:11:59 +0100

    feat: add HandOfCards class

---
+++ src/main/java/edu/ntnu/idatt2001/trym/cardgame/cardgame/backend/HandOfCards.java
@@ -0,0 +1,15 @@
+package edu.ntnu.idatt2001.trym.cardgame.cardgame.backend;
+
+import java.util.List;
+
+/**
+ * This class outlines the information and methods that are intrinsic to a hand of cards. Therefore, the hand of cards
+ * has been represented using an arrayList of PlayingCards.
+ */
+public class HandOfCards {
+    private List<PlayingCard> handOfCards;
+
+    public HandOfCards(List<PlayingCard> handOfCards) {
+        this.handOfCards = handOfCards;
+    }
+}
