9e246c065fa0da29a41e1d5deb1dce6e5d9f0f14
Trym Gudvangen <trym.gudvangen@gmail.com>
2022-03-26 16:31:28 +0100

    fix: change dealHand method

--- src/main/java/edu/ntnu/idatt2001/trym/cardgame/cardgame/backend/DealerTable.java
+++ src/main/java/edu/ntnu/idatt2001/trym/cardgame/cardgame/backend/DealerTable.java
@@ -3,6 +3,7 @@ package edu.ntnu.idatt2001.trym.cardgame.cardgame.backend;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.IntStream;
 
 /**
  * This class represents a dealer table, where a card game is currently being held. It, therefore, contains information
@@ -22,16 +23,25 @@ public class DealerTable {
      * This method deals a hand of size n, as long as there are an n-number of cards left in the deck. If n is
      * an invalid number, smaller than 1 or more than the deck size, then an IllegalArgumentException is thrown.
      * @param n Number of cards desired, represented as an int
-     * @return Hand of size n, represented as a List of randomly chosen PlayingCards.
+     * @return  Hand of size n, represented as a List of randomly chosen PlayingCards.
      */
     public List<PlayingCard> dealHand(int n){
         List<PlayingCard> hand = new ArrayList<>();
         if(n < 1 || n > this.tableDeck.getDeckOfCards().size()) throw new IllegalArgumentException("The hand size is invalid!");
-        for(int i = 0; i < n; i++){
+
+//        for(int i = 0; i < n; i++){
+//            int randomNum = random.nextInt(this.tableDeck.getDeckOfCards().size());
+//            hand.add(this.tableDeck.getDeckOfCards().get(randomNum));
+//            this.tableDeck.getDeckOfCards().remove(randomNum);
+//        }
+
+        IntStream.range(0, n).forEach(i -> {
             int randomNum = random.nextInt(this.tableDeck.getDeckOfCards().size());
             hand.add(this.tableDeck.getDeckOfCards().get(randomNum));
             this.tableDeck.getDeckOfCards().remove(randomNum);
-        }
+        });
+
         return hand;
     }
+    //TODO: add unit test for this method and constructor.
 }
